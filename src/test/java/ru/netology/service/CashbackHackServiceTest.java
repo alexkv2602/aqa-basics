6f5255050629fc87860d231e9e4c2884791d4894
AlexKV <bloodklesh@gmail.com>
2023-02-02 01:39:16 +0300

    junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,31 @@
-import static org.testng.Assert.*;
+package ru.netology.service;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class CashbackHackServiceTest {
-    
+
+    @Test
+    public void testRemainPositive() {
+        int amount = 900;
+        int expected = 100;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void testRemainCrash() {
+        int amount = 1000;
+        int expected = 0;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+
+    }
 }
